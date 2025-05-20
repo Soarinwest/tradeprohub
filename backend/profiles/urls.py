9bd889b75d62d886377cd391cc2d05a32e05a35c
Soren <soren.donisvitch@gmail.com>
2025-05-20 17:30:28 -0400

    update

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,16 +1,9 @@
-# backend/profiles/urls.py
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import (
-    RegisterView,
-    EmployeeProfileCreateView,
-    EmployeeProfileDetailView
-)
+# backend/backend/urls.py
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('employee-profile/', EmployeeProfileCreateView.as_view(), name='employee-profile-create'),
-    path('my-employee-profile/', EmployeeProfileDetailView.as_view(), name='my-employee-profile'),
+    path('admin/', admin.site.urls),
+    path('api/', include('profiles.urls')),
 ]
+
